87e2e5bb5461a2708fbe0f58c929e729a504025f
TristanPinaudeau <tristan@flexfactory.fr>
2019-02-15 19:05:47 +0100

    Rebuild vms

--- start_lab.sh
+++ start_lab.sh
@@ -5,5 +5,5 @@ virsh start nextcloud
 virsh start next_backup
 virt-viewer nextcloud
 virt-viewer next_backup
-tee -a /etc/hosts <<< '10.0.0.230 nextcloud.lab'
-tee -a /etc/hosts <<< '10.0.0.152 backup.lab'
+tee -a /etc/hosts <<< '10.0.0.167 nextcloud.lab'
+tee -a /etc/hosts <<< '10.0.0.222 backup.lab'
