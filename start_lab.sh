fb475b22b92203d3d62b9bf70d03ef42d65a087b
TristanPinaudeau <tristan@flexfactory.fr>
2019-02-15 19:13:11 +0100

    Rename vms

--- start_lab.sh
+++ start_lab.sh
@@ -2,8 +2,8 @@
 
 virsh net-start preprod1
 virsh start nextcloud
-virsh start next_backup
+virsh start backup
 virt-viewer nextcloud
-virt-viewer next_backup
+virt-viewer backup
 tee -a /etc/hosts <<< '10.0.0.167 nextcloud.lab'
 tee -a /etc/hosts <<< '10.0.0.222 backup.lab'
