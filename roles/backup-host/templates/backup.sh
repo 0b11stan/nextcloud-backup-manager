0cbefd51e8579efc669796250fb7871135a3b478
TristanPinaudeau <tristan@flexfactory.fr>
2019-02-16 16:13:22 +0100

    Clean backup done thanks to sudoer config

--- roles/backup-host/templates/backup.sh
+++ roles/backup-host/templates/backup.sh
@@ -3,18 +3,18 @@
 dbname={{database.name}}
 dbuser={{database.user}}
 dbpassword={{database.password}}
-databasebackup=nextcloud-db-`date +"%Y%m%d"`.sql
-filesystembackup=nextcloud-fs-`date +"%Y%m%d"`
+databasebackup=nextcloud-`date +"%Y%m%d"`.sql
+filesystembackup=nextcloud-`date +"%Y%m%d"`
 
 # put nextcloud instance to maintenance mode
-ssh {{backup_user}}@{{nextcloud_host}} php occ maintenance:mode --on
+ssh {{backup_user}}@{{nextcloud_host}} 'sudo -u www-data /usr/bin/php /var/www/html/nextcloud/occ maintenance:mode --on'
 
 # dump the database
 ssh {{backup_user}}@{{nextcloud_host}} "mysqldump --single-transaction -u $dbuser -p$dbpassword $dbname > ~/$databasebackup"
 
 # copy datas from the backup server
-rsync -avx {{backup_user}}@{{nextcloud_host}}:/var/www/html/nextcloud/ /backups/$filesystembackup/
-rsync -avx {{backup_user}}@{{nextcloud_host}}:/home/{{backup_user}}/$databasebackup /backups/$databasebackup
+rsync -avx {{backup_user}}@{{nextcloud_host}}:/var/www/html/nextcloud/ /data/backup/filesystem/$filesystembackup/
+rsync -avx {{backup_user}}@{{nextcloud_host}}:/home/{{backup_user}}/$databasebackup /data/backup/database/$databasebackup
 
 # put nextcloud back to normal mode
 ssh {{backup_user}}@{{nextcloud_host}} php occ maintenance:mode --off
